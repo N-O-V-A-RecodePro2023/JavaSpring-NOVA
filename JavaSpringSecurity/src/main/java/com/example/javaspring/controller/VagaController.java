99abe3e9d936dda0a6e5430aa07c53bfea03015e
mikemars0701 <maycon_martins2017@hotmail.com>
2024-01-25 14:44:26 -0300

    Refatoração, detalhes de rotas, revisão geral

--- JavaSpringSecurity/src/main/java/com/example/javaspring/controller/VagaController.java
+++ JavaSpringSecurity/src/main/java/com/example/javaspring/controller/VagaController.java
@@ -1,11 +1,16 @@
 package com.example.javaspring.controller;
 
 import org.springframework.ui.Model;
+
+
 import com.example.javaspring.model.Vaga;
+
 import com.example.javaspring.services.VagaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+
 
 import java.util.List;
 
@@ -17,13 +22,18 @@ public class VagaController {
     @Autowired
     private VagaService vagaService;
 
-
+    
+    
     //Persistência
     @PostMapping("/cadastro")
     public String saveVaga(@ModelAttribute ("vaga") Vaga vaga){
         Vaga savedVaga = vagaService.saveVaga(vaga);
-        return "redirect:/vagas/vagas/";
-    }
+        return "redirect:/vagas/vagas";
+        
+      
+    } 
+    
+    
 
     //Ver a vaga por id
     @GetMapping("/vaga/{idVaga}")
@@ -45,11 +55,11 @@ public class VagaController {
     @GetMapping("/vagas")
     public String showVagas(Model model){
         List<Vaga> vaga = vagaService.getAllVagas();
-        model.addAttribute("vaga", vaga);
+        model.addAttribute("vagas", vaga);
         return "vagas";
     }
     
-
+    
     //Mostrar o form de edição
     @GetMapping("/editar/{idVaga}")
     public String ShowFormUpdate(@PathVariable Integer idVaga, Model model){
@@ -61,14 +71,14 @@ public class VagaController {
     @PostMapping("/editar/{idVaga}")
     public String updateVaga(@PathVariable Integer idVaga, @ModelAttribute ("vaga") Vaga vaga){
         vagaService.updateVaga(idVaga, vaga);
-        return "redirect:/vagas/vaga/" + idVaga;
+        return "redirect:/vagas/vaga" + idVaga;
     }
 
     //Deletar vaga
     @GetMapping("/deletar/{idVaga}")
     public String deleteVaga(@PathVariable Integer idVaga){
         vagaService.deleteVaga(idVaga);
-        return "redirect:/home.html";
+        return "redirect:/index.html";
     }
 
 
